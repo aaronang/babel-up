92e47287b86580435e71128f653b6841c6c135dc
Aaron Ang <awz.ang@gmail.com>
2017-03-26 21:24:05 -0700

    Remove command-exists and clone Babel

--- src/babel-up.js
+++ src/babel-up.js
@@ -1,6 +1,29 @@
-import { sync as commandExists } from "command-exists";
+import Git from "nodegit";
+import fs from "fs";
+import path from "path";
 
-if (!commandExists("yarn")) {
-  console.error("⚠️  Babel UP requires Yarn to be installed.");
-  process.exit(1);
+const UP_PATH = "/tmp/babel-up";
+const BABEL_PATH = path.join(UP_PATH, "babel");
+
+if (fs.existsSync(BABEL_PATH)) {
+  let repository;
+  Git.Repository.open(BABEL_PATH)
+    .then((repo) => {
+      repository = repo;
+      return repository.fetchAll();
+    })
+    .then(() => {
+      repository.mergeBranches("7.0", "origin/7.0");
+    })
+    .done(() => {
+      console.log("✨  Repository updated successfully.");
+    });
+} else {
+  Git.Clone("https://github.com/babel/babel.git", BABEL_PATH)
+    .then(() => {
+      console.log("✨  Repository cloned successfully.");
+    })
+    .catch((error) => {
+      console.error(`⚠️  ${error.message}.`);
+    });
 }
