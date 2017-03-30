c9c665e9f0eea0d4c6e1da30af92025c74717741
Aaron Ang <awz.ang@gmail.com>
2017-03-29 22:00:28 -0700

    ✨  Get temporary directory

--- src/paths.js
+++ src/paths.js
@@ -1,5 +1,7 @@
 import path from "path";
+import os from "os";
 
-export const BABEL_UP_PATH = "/tmp/babel-up";
+export const TEMP_PATH = os.tmpdir();
+export const BABEL_UP_PATH = path.join(TEMP_PATH, "babel-up");
 export const BABEL_PATH = path.join(BABEL_UP_PATH, "babel");
 export const BABEL_PACKAGES_PATH = path.join(BABEL_PATH, "packages");
