9b11bbb462a198a2e523e5110cb54b189b8fd819
Aaron Ang <awz.ang@gmail.com>
2017-04-02 11:38:52 -0700

    📐  `yarn run test`

--- src/babel-up.js
+++ src/babel-up.js
@@ -1,11 +1,19 @@
 import { getRepositories } from "./git";
 import { linkAll, unlinkAll } from "./yarn";
+import { spawn } from "child_process";
 
 getRepositories()
   .then(() => {
     console.log("🏁  Finished cloning and updating Babel repositories.");
-  })
-  .then(() => {
+
     linkAll();
-    unlinkAll();
+
+    const test = spawn("yarn", ["run", "test"]);
+    test.stdout.on("data", (data) => {
+      console.log(data.toString());
+    });
+    test.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+      unlinkAll();
+    });
   });
